1a320a6ef74679f5cc890d85ba6388c6e361a538
alina-valenda <157399209+alina-valenda@users.noreply.github.com>
2024-09-23 12:23:47 +0200

    module7-task1 (#18)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,8 @@
-/* в этот файл добавляет скрипты*/
+const navToggle = document.querySelector('.page-nav__toggle');
+const navList = document.querySelector('.page-nav__list');
+navToggle.classList.remove('page-nav__toggle--opened');
+navList.classList.remove('page-nav__list--opened');
+navToggle.onclick = function () {
+  navToggle.classList.toggle('page-nav__toggle--opened');
+  navList.classList.toggle('page-nav__list--opened');
+};
